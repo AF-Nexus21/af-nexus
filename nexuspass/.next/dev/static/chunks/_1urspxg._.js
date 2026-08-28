8e6cc6b3d1c235616e849298627ce628d315d0b4
AF-Nexus21 <fensgie@gmail.com>
2026-08-28 12:54:59 +0800

    Itinayo ang AF-NEXUS Landing Page

--- nexuspass/.next/dev/static/chunks/_1urspxg._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/_19auf3r._.js","static/chunks/node_modules_0x_m_hm._.js","static/chunks/_0-5fqhz._.js"],
-    source: "entry"
-});
