1b16eb5a51a5b9cd60b9935efeb47970f40e052d
AF-Nexus21 <fensgie@gmail.com>
2026-08-28 20:09:04 +0800

    Updated Live Display with 2:3 photo ratio

--- app/intrams/display/page.tsx
+++ app/intrams/display/page.tsx
@@ -155,7 +155,7 @@ export default function DisplayPage() {
                         <img
                           src={candidate.photo_url || "/default-avatar.png"}
                           alt={candidate.name}
-                          className="h-24 w-16 object-cover" // 2:3 ratio (Height 24, Width 16)
+                          className="h-24 w-16 object-cover"
                         />
                         <span>{candidate.name}</span>
                       </div>
